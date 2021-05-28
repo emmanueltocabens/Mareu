acd305b9c26efb748d17c4bbbd894f99c3fd7a9d
Emmanuel Tocabens <tocab97@gmail.com>
2021-05-28 18:28:08 +0200

    tests

---
+++ app/src/androidTest/java/com/lamzone/mareu/ui/MeetingListUI.java
@@ -0,0 +1,131 @@
+package com.lamzone.mareu.ui;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+
+import androidx.test.core.app.ActivityScenario;
+import androidx.test.espresso.ViewInteraction;
+import androidx.test.espresso.action.ViewActions;
+import androidx.test.espresso.assertion.ViewAssertions;
+import androidx.test.espresso.matcher.ViewMatchers;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import com.lamzone.mareu.DI.DependencyInjector;
+import com.lamzone.mareu.R;
+import com.lamzone.mareu.service.MareuApiService;
+import com.lamzone.mareu.utils.RecyclerViewUtils;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.scrollTo;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withClassName;
+import static androidx.test.espresso.matcher.ViewMatchers.withContentDescription;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withParent;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.is;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.Matchers;
+import org.hamcrest.TypeSafeMatcher;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+
+/**
+ * Unit Tests
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class MeetingListUI {
+
+    private int currentMeetingSize = 6;
+    private MareuApiService apiService;
+
+
+    @Rule
+    public ActivityScenarioRule<MareuListActivity> mActivityRule = new ActivityScenarioRule<>(MareuListActivity.class);
+
+    @Before
+    public void init(){
+        ActivityScenario<MareuListActivity> scenario = mActivityRule.getScenario();
+        apiService = DependencyInjector.getNewInstanceApiService();
+    }
+
+    @Test
+    public void checkIfRecyclerViewIsFilled(){
+        onView(ViewMatchers.withId(R.id.recyclerView))
+                .check(new RecyclerViewUtils.ItemCount(currentMeetingSize));
+    }
+
+    //créé par espresso
+    @Test
+    public void dateFilterIsWorking(){
+
+        ViewInteraction overflowMenuButton = onView(
+                allOf(withContentDescription("More options"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.action_bar),
+                                        1),
+                                0),
+                        isDisplayed()));
+        overflowMenuButton.perform(click());
+
+        ViewInteraction materialTextView = onView(
+                allOf(withId(R.id.title), withText("Filtrer par date"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.content),
+                                        0),
+                                0),
+                        isDisplayed()));
+        materialTextView.perform(click());
+
+        ViewInteraction materialButton = onView(
+                allOf(withId(android.R.id.button1), withText("OK"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.ScrollView")),
+                                        0),
+                                3)));
+        materialButton.perform(scrollTo(), click());
+    }
+
+    @Test
+    public void navigateToAddMeetingIsWorking(){
+        onView(withId(R.id.fab_add_meeting)).perform(ViewActions.click());
+        onView(withId(R.id.add_et_title)).check(ViewAssertions.matches(ViewMatchers.isDisplayed()));
+    }
+
+
+    /**
+     * généré par espresso
+     */
+    private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
+    }
+}
